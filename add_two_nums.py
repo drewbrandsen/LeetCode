893de7078367bc4d6db3369e55a5cd54ef81ff84
Drew Brandsen <drew.brandsen@gmail.com>
2020-05-18 22:39:43 -0500

    Passing solution of add_two_nums

--- add_two_nums.py
+++ add_two_nums.py
@@ -5,49 +5,54 @@ class ListNode:
         self.next = next
 
 
-def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
 
-    # Check for empty val - assuming both lists are the same length
-    value = l1.val + l2.val
-    carryover = 0
+        # Check for empty val - assuming both lists are the same length
+        value = l1.val + l2.val
+        carryover = 0
 
-    # Check for "rollover"
-    if value > 9:
-        value = value % 10
-        carryover = 1
+        # Check for "rollover"
+        if value > 9:
+            value = value % 10
+            carryover = 1
 
-    # If both lists are empty, at the base case
-    if (l1.next is None) and (l2.next is None):
-        return_list = ListNode(value)
+        # If both lists are empty, at the base case
+        if (l1.next is None) and (l2.next is None):
+            if carryover == 1:
+                return_list = ListNode(value, ListNode(1))
+            else:
+                return_list = ListNode(value)
 
-    # Need more recursion
-    else:
-        # Left side empty
-        if l1.next is None:
-            # Add the carry over value as a single node to left side
-            l1_next = ListNode(carryover)
-            # Recurse!
-            return_list = ListNode(value, addTwoNumbers(l1_next, l2.next))
-
-        # Right side empty
-        elif l2.next is None:
-            # Add the carry over value as a single node to the right side
-            l2_next = ListNode(carryover)
-            # Recurse!
-            return_list = ListNode(value, addTwoNumbers(l1.next, l2_next))
-
-        # Both Lists non-empty
+        # Need more recursion
         else:
-            # Add the carry over value to the first list
-            l1_next = l1.next
-            l1_next.val = l1_next.val + carryover
-            # Recurse!
-            return_list = ListNode(value, addTwoNumbers(l1_next, l2.next))
+            # Left side empty
+            if l1.next is None:
+                # Add the carry over value as a single node to left side
+                l1_next = ListNode(carryover)
+                # Recurse!
+                return_list = ListNode(value, Solution.addTwoNumbers(self, l1_next, l2.next))
 
-    return return_list
+            # Right side empty
+            elif l2.next is None:
+                # Add the carry over value as a single node to the right side
+                l2_next = ListNode(carryover)
+                # Recurse!
+                return_list = ListNode(value, Solution.addTwoNumbers(self, l1.next, l2_next))
+
+            # Both Lists non-empty
+            else:
+                # Add the carry over value to the first list
+                l1_next = l1.next
+                l1_next.val = l1_next.val + carryover
+                # Recurse!
+                return_list = ListNode(value, Solution.addTwoNumbers(self, l1_next, l2.next))
+
+        return return_list
 
 
 if __name__ == "__main__":
+    runSolution = Solution()
     # Test Cases
     # Basic test no rollovers, same length
     # Explanation: 343 + 455 = 798.
@@ -60,7 +65,7 @@ if __name__ == "__main__":
     l2 = ListNode(4, l2)
 
     test_str = "No Rollovers - same length"
-    answer = addTwoNumbers(l1, l2)
+    answer = runSolution.addTwoNumbers(l1, l2)
     assert answer.val == 7
     answer = answer.next
     assert answer.val == 9
@@ -81,7 +86,7 @@ if __name__ == "__main__":
     l2 = ListNode(4, l2)
 
     test_str = "Rollovers - same length"
-    answer = addTwoNumbers(l1, l2)
+    answer = runSolution.addTwoNumbers(l1, l2)
     assert answer.val == 7
     answer = answer.next
     assert answer.val == 0
@@ -98,7 +103,7 @@ if __name__ == "__main__":
     l2 = ListNode(4, l2)
 
     test_str = "No Rollovers - different length"
-    answer = addTwoNumbers(l1, l2)
+    answer = runSolution.addTwoNumbers(l1, l2)
     assert answer.val == 6
     answer = answer.next
     assert answer.val == 5
@@ -106,21 +111,14 @@ if __name__ == "__main__":
     assert answer.val == 5
     print("Passes: " + test_str)
 
-    # Rollovers and different length
-    # 900 + 455 = 1355
-    l1 = ListNode(9)
-
+    # Digit rollover
+    # 5 + 5 = 10
+    l1 = ListNode(5)
     l2 = ListNode(5)
-    l2 = ListNode(5, l2)
-    l2 = ListNode(4, l2)
 
-    test_str = "No Rollovers - different length"
-    answer = addTwoNumbers(l1, l2)
+    test_str = "Digit rollover"
+    answer = runSolution.addTwoNumbers(l1, l2)
+    assert answer.val == 0
+    answer = answer.next
     assert answer.val == 1
-    answer = answer.next
-    assert answer.val == 3
-    answer = answer.next
-    assert answer.val == 5
-    answer = answer.next
-    assert answer.val == 5
     print("Passes: " + test_str)
