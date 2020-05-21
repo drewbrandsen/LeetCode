dfff4bd9860f4868442e9f35076ae5c479771b86
DrewBrandsen <drew.brandsen@gmail.com>
2020-05-20 20:43:18 -0500

    Basic algorithm in place using dictionaries with a single pass.

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -2,13 +2,28 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Key is detecting repeated letters
         letter_dict = {}
+        max_substring = 0
 
         for idx, char in enumerate(s):
-            print(idx)
+            # Place every char into the dictionary
+            if char not in letter_dict.keys():
+                letter_dict[char] = idx
 
-        return 5
+            # If the char already exists in the dictionary
+            else:
+                # Get length of found substring (repeated char means end
+                # of substring found)
+                len_substring = idx - letter_dict[char]
 
+                # Update index for this char since substring starts over
+                letter_dict[char] = idx
 
+                # If new substring is the record, update max
+                if len_substring > max_substring:
+                    max_substring = len_substring
+
+        # Return max substring length
+        return max_substring
 
 
 if __name__ == "__main__":
@@ -17,17 +32,17 @@ if __name__ == "__main__":
     # Test 1
     input_str = "abcabcbb"
     ans = solution.lengthOfLongestSubstring(input_str)
-    assert(ans) == 3
+    assert(ans == 3)
     print("Test 1 - Passed: " + input_str)
 
     # Test 1
     input_str = "bbbbb"
     ans = solution.lengthOfLongestSubstring(input_str)
-    assert(ans) == 1
+    assert(ans == 1)
     print("Test 2 - Passed: " + input_str)
 
     # Test 1
     input_str = "pwwkew"
     ans = solution.lengthOfLongestSubstring(input_str)
-    assert(ans) == 3
+    assert(ans == 3)
     print("Test 3 - Passed: " + input_str)
