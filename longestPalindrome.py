b2c0cd0ae00ea14ede0c0cacb1db19fdb1047f2c
DrewBrandsen <drew.brandsen@gmail.com>
2020-05-31 21:12:42 -0500

    working version that passes unit tests

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,35 +1,50 @@
 class Solution:
+
+    def check_palindrome(self, s: str) -> int:
+
+        len_string = len(s)
+
+        if len_string % 2:  # odd string length
+            loop_range = int((len_string-1) / 2)
+            result_len = 1  # middle char always a match
+        else:
+            loop_range = int(len_string / 2)
+            result_len = 0
+
+        for i in range(loop_range):
+            if s[i] == s[-i-1]:
+                result_len += 2
+            else:
+                return -1  # not a palindrome
+
+        return result_len
+
     def longestPalindrome(self, s: str) -> str:
+
         str_palindrome = ""
         len_palindrome = 0
 
-        # Option 2: for every character, search outward for "palindromicity"
+        # Base case
+        if len(s) == 1:
+            return s
+
+        # Loop over every character, this character could be the start
+        # of a palindrome, so check other instances of this character
+        # in the string and see if it forms the endpoints of a palindrome
+
+        # Loop over every character
         for idx, ch in enumerate(s):
-            if idx == 0:  # base case
-                str_palindrome = ch
-                len_palindrome = len(str_palindrome)
-            else:
-                max_length = min(idx-0, len(s)-idx)
-                # Even case (middle + right elements equal)
-                if ch == s[idx+1]:
-                    print("even case")
+            indexes_ch = [i for i, letter in enumerate(s) if letter == ch]
 
-                # Odd Case
-                elif s[idx-1] == s[idx+1]:
-                    # Fold string at the character of index
-                    l_side = s[idx-max_length:idx]
-                    r_side = s[idx+max_length:idx:-1]
-
-                    for i in range(max_length):
-                        if l_side[0:i+1] == r_side[0:i+1]:
-                            if i+1 > len_palindrome:
-                                str_palindrome = s[(idx-i-1):(idx+i+2)]
-                                len_palindrome = len(str_palindrome)
-                        else:
-                            break
-
-                else:
-                    continue  # no palindrom with this char at middle
+            # Loop over each substring
+            for char_idx in indexes_ch:
+                # Only look forward for substrings
+                if idx < char_idx:
+                    substring = s[idx:char_idx + 1]
+                    result_len = self.check_palindrome(substring)
+                    if result_len > len_palindrome:
+                        len_palindrome = result_len
+                        str_palindrome = substring
 
         return str_palindrome
 
@@ -48,3 +63,9 @@ if __name__ == "__main__":
     ans = solution.longestPalindrome(input_str)
     assert(ans == "bb")
     print("Test 2 - Passed: '" + input_str + "'")
+
+    # Test 3
+    input_str = "a"
+    ans = solution.longestPalindrome(input_str)
+    assert(ans == "a")
+    print("Test 3 - Passed: '" + input_str + "'")
